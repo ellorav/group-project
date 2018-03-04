489d4944c7466c142b18557d4a5a60920f8a4039
Ellora Virtue <ellora.virtue@hotmail.com>
2018-03-04 16:03:14 +0000

    First attempt
    dkvsldkvn

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('h1').css('color', '#f00');
+
+  $('h1').click(function() {
+    $(this).css('color', '#ff0');
+  })
+
+  /*$("#button").click(function() {
+    var text = $('#input').val();
+    $("list").append('<li>' + text + '</li>');
+  });*/
+
+});
